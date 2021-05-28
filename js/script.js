f71c69a7b159dc5765e9d1e91119171f4e4119ae
CarloD39 <carlodurand@hotmail.it>
2021-05-28 17:14:05 +0200

    treno che funziona

--- js/script.js
+++ js/script.js
@@ -5,17 +5,18 @@ var ticket = 0.21 ;
 
 
 
-if (parseInt(age) < 18);{
+if (parseInt(age) < 18){
     var sconto = (ticket * 20) / 100;
     var ticket = ticket - sconto;
     var prezzo = parseInt(km) * ticket;
     document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
     
-// } if else (parseInt(age) > 65);{ 
-// var sconto = (ticket * 40) / 100;
-// var ticket = ticket - sconto;
-// var prezzo = parseInt(km) * ticket;
-// document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
+} 
+else if (parseInt(age) > 65){ 
+var sconto = (ticket * 40) / 100;
+var ticket = ticket - sconto;
+var prezzo = parseInt(km) * ticket;
+document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
 
 }
 
