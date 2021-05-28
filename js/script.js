5b56c739ac28ae1fbc3693b43efcb1a9152fc09f
CarloD39 <carlodurand@hotmail.it>
2021-05-28 17:04:02 +0200

    train

---
+++ js/script.js
@@ -0,0 +1,27 @@
+var km = prompt('quanti chilometri di percorrenza?');
+var age = prompt('inserire età passegero');
+
+var ticket = 0.21 ;
+
+
+
+if (parseInt(age) < 18);{
+    var sconto = (ticket * 20) / 100;
+    var ticket = ticket - sconto;
+    var prezzo = parseInt(km) * ticket;
+    document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
+    
+// } if else (parseInt(age) > 65);{ 
+// var sconto = (ticket * 40) / 100;
+// var ticket = ticket - sconto;
+// var prezzo = parseInt(km) * ticket;
+// document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
+
+}
+
+
+
+
+    
+
+
