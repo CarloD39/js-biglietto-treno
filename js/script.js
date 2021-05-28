fcdc7110082268518148d66c6479f169255f6c06
CarloD39 <carlodurand@hotmail.it>
2021-05-28 17:23:05 +0200

    treno3

--- js/script.js
+++ js/script.js
@@ -3,20 +3,21 @@ var age = prompt('inserire età passegero');
 
 var ticket = 0.21 ;
 
-
-
 if (parseInt(age) < 18){
     var sconto = (ticket * 20) / 100;
     var ticket = ticket - sconto;
     var prezzo = parseInt(km) * ticket;
     document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
     
-} 
-else if (parseInt(age) > 65){ 
-var sconto = (ticket * 40) / 100;
-var ticket = ticket - sconto;
-var prezzo = parseInt(km) * ticket;
-document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
+} else if (parseInt(age) > 65){ 
+    var sconto = (ticket * 40) / 100;
+    var ticket = ticket - sconto;
+    var prezzo = parseInt(km) * ticket;
+    document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
+
+}else{
+    var prezzo = ticket * parseInt(km);
+    document.getElementById('FinalPrice').innerHTML = 'il final price è ' + prezzo;
 
 }
 
